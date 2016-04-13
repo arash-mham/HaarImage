1904a3e9b3e2fbc08beba4022e0adc05c976aff6
arash_mham <a.mahdavi.amiri@gmail.com>
2016-04-12 21:01:11 -0700

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace HaarImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Load = new System.Windows.Forms.Button();
+            this.Haar = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(2, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(255, 225);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Load
+            // 
+            this.Load.Location = new System.Drawing.Point(13, 244);
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(75, 23);
+            this.Load.TabIndex = 1;
+            this.Load.Text = "Load";
+            this.Load.UseVisualStyleBackColor = true;
+            this.Load.Click += new System.EventHandler(this.Load_Click);
+            // 
+            // Haar
+            // 
+            this.Haar.Location = new System.Drawing.Point(94, 244);
+            this.Haar.Name = "Haar";
+            this.Haar.Size = new System.Drawing.Size(75, 23);
+            this.Haar.TabIndex = 2;
+            this.Haar.Text = "Haar";
+            this.Haar.UseVisualStyleBackColor = true;
+            this.Haar.Click += new System.EventHandler(this.Haar_Click);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(175, 244);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 3;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 295);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.Haar);
+            this.Controls.Add(this.Load);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Haar";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Load;
+        private System.Windows.Forms.Button Haar;
+        private System.Windows.Forms.Button Save;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace HaarImage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Load_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Open Image";
+                dlg.Filter = "bmp files (*.png)|*.png";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {               
+                    pictureBox1.Image = new Bitmap(dlg.FileName);
+                }
+            }
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Haar_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                Bitmap OriginalBitmap = new Bitmap(pictureBox1.Image);
+                int OriginWidth = (OriginalBitmap.Width / 3) * 3;
+                int OriginHeight = (OriginalBitmap.Height / 3) * 3;
+                Bitmap RowCompressedBitmap = new Bitmap(OriginHeight, pictureBox1.Image.Height / 3);
+                Bitmap CompressedBitmap = new Bitmap(pictureBox1.Image.Width / 3, pictureBox1.Image.Height / 3);
+               
+                for (int i = 0; i < OriginWidth; i++)
+                {
+                    for (int j = 0; j < OriginHeight; j += 3)
+                    {
+                        byte R = (byte)(((int)OriginalBitmap.GetPixel(i, j).R + (int)OriginalBitmap.GetPixel(i, j + 1).R + (int)OriginalBitmap.GetPixel(i, j + 2).R) / 3);
+                        byte G = (byte)(((int)OriginalBitmap.GetPixel(i, j).G + (int)OriginalBitmap.GetPixel(i, j + 1).G + (int)OriginalBitmap.GetPixel(i, j + 2).G) / 3);
+                        byte B = (byte)(((int)OriginalBitmap.GetPixel(i, j).B + (int)OriginalBitmap.GetPixel(i, j + 1).B + (int)OriginalBitmap.GetPixel(i, j + 2).B) / 3);
+                        Color myRgbColor = new Color();
+                        myRgbColor = Color.FromArgb(R, G, B);
+                        RowCompressedBitmap.SetPixel(i, j / 3, myRgbColor);
+                    }
+                }
+                for (int i = 0; i < RowCompressedBitmap.Width ; i += 3)
+                {
+                    for (int j = 0; j < RowCompressedBitmap.Height; j++)
+                    {
+                        byte R = (byte)(((int)RowCompressedBitmap.GetPixel(i, j).R + (int)RowCompressedBitmap.GetPixel(i + 1, j).R + (int)RowCompressedBitmap.GetPixel(i + 2, j).R) / 3);
+                        byte G = (byte)(((int)RowCompressedBitmap.GetPixel(i, j).G + (int)RowCompressedBitmap.GetPixel(i + 1, j).G + (int)RowCompressedBitmap.GetPixel(i + 2, j).G) / 3);
+                        byte B = (byte)(((int)RowCompressedBitmap.GetPixel(i, j).B + (int)RowCompressedBitmap.GetPixel(i + 1, j).B + (int)RowCompressedBitmap.GetPixel(i + 2, j).B) / 3);
+                        Color myRgbColor = new Color();
+                        myRgbColor = Color.FromArgb(R, G, B);
+                        CompressedBitmap.SetPixel(i / 3, j, myRgbColor);
+                    }
+                }
+                pictureBox1.Image = new Bitmap(CompressedBitmap);
+            }
+            
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog save_image = new SaveFileDialog())
+            {
+                save_image.ShowDialog();
+                string name = save_image.FileName;
+                pictureBox1.Image.Save(name, System.Drawing.Imaging.ImageFormat.Png);
+            }
+        }
+
+       }
+}
